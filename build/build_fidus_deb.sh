441c28ecc31bfa0b804d01d3caf1c91fbe80900b
mohdje <you@example.com>
2026-05-28 10:58:26 +0100

    create dedicated build folder

---
+++ build/build_fidus_deb.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+# Build Release self-contained version for linux-x64
+dotnet publish ../Fidus/Fidus.csproj -c Release -r linux-x64 --self-contained true
+
+rm -rf ./publish/usr/
+
+mkdir -p ./publish/usr/bin/fidusterminal
+
+# Copy the built files to build/publish
+cp ../Fidus/bin/Release/net10.0/linux-x64/publish/* ./publish/usr/bin/fidusterminal/
+
+# Rename the main executable to "fidus"
+mv ./publish/usr/bin/fidusterminal/Fidus ./publish/usr/bin/fidusterminal/fidus
+
+# Build the deb package
+dpkg-deb --build ./publish  ./fidus.deb
+
+# Create a zip archive of the deb package
+zip -j ./fidus.deb.zip ./fidus.deb
+
+echo "Build and packaging complete. Output: ./fidus.deb, ./fidus.deb.zip"
